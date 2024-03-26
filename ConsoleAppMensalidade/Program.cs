d8941a1228a403a3e01ac2c6dffda4d8f7957cdb
Usuario <Usuario@CPD02-M02>
2024-03-26 11:37:30 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppMensalidade/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMensalidade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 1;
+            Console.WriteLine("Qual o valor da anuidade: ");
+            double anui = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o valor da matrícula: ");
+            double matri = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o valor do material: ");
+            double mater = double.Parse(Console.ReadLine());
+
+            while(cont <= 12)
+            {
+                double total = anui / 12 + matri + mater;
+                matri = 0;
+                mater = 0;
+                Console.WriteLine("Mensalidade: " +cont+ " valor: "+total.ToString("C"));
+                cont++;
+            }
+            Console.ReadKey();
+        } 
+    }
+}
